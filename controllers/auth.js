e393c05c9ed04d92e9b2c27a6c87af71a4c3211d
nazir99 <95626025+nazir99@users.noreply.github.com>
2022-02-01 12:20:17 +0700

    Complete Sign Up Process

--- controllers/auth.js
+++ controllers/auth.js
@@ -33,21 +33,30 @@ router.post('/signup', async (req, res, next) => {
   try {
     // code here
     // 1 check that user is not in db
-    let user = await Users.create(req.body)
-    // LOG user
-    console.log({ user })
-    // if user
-    // 1. login
-    // 2. redirect
-    // else
-    // throw error
-    req.login(user, err => {
-      if (err) {
-        throw err
-      }
-      // continue coding here
-      res.redirect('/houses')
-    })
+    let foundUser = await Users.findOne({ email: req.body.email })
+    console.log({ foundUser })
+    if (foundUser) {
+      throw new Error(
+        'User with this email already exists, please use another email'
+      )
+    } else {
+      let user = await Users.create(req.body)
+      // LOG user
+      console.log({ user })
+      // if user
+      // 1. login
+      // 2. redirect
+      // else
+      // throw error
+      req.login(user, err => {
+        if (err) {
+          throw err
+        }
+        // continue coding here
+        res.redirect('/houses')
+      })
+    }
+
     // errors
     // throw new Error('message')
   } catch (err) {
